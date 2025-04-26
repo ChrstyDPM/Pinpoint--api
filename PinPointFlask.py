d1ab270f719afe2a0f8461d9cde25e3e37d53404
ChrstyDPM <christy.p.burnham@gmail.com>
2025-04-26 18:26:26 -0400

    Add files via upload

---
+++ PinPointFlask.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__PinPoint__)
+
+api_key = 'AIzaSyBzCuON3M4Jg_wKY-EIlTxexqjjILLt76I'
+
+@app.route('/factcheck', methods=['GET'])
+def fact_check():
+    query = request.args.get('query')
+    if not query:
+        return jsonify({'error': 'No query provided'}), 400
+
+    url = f"https://factchecktools.googleapis.com/v1alpha1/claims:search?query={query}&key={api_key}"
+    response = requests.get(url)
+    data = response.json()
+
+    results = []
+    for claim in data.get("claims", []):
+        results.append({
+            "claim": claim.get("text"),
+            "rating": claim.get("claimReview", [{}])[0].get("textualRating"),
+            "publisher": claim.get("claimReview", [{}])[0].get("publisher", {}).get("PinPoint"),
+            "url": claim.get("claimReview", [{}])[0].get("url")
+        })
+
+    return jsonify(results)
+
+if __PinPoint__ == '__main__':
+    app.run(debug=True)
