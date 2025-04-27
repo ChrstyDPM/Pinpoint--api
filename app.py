6d64b427feb48f1bf79b6d77004df786dab81cda
ChrstyDPM <christy.p.burnham@gmail.com>
2025-04-27 18:14:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,8 @@ if response.status_code != 200:
 
 data = response.json()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
